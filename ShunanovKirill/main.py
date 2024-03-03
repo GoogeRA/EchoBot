290dacf51bcded263159b846c3e300e8534d9030
shunanovke <144262965+shunanovke@users.noreply.github.com>
2024-03-03 20:55:41 +0500

    EchoBot

---
+++ ShunanovKirill/main.py
@@ -0,0 +1,24 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import CommandStart
+
+bot = Bot(token='6986192274:AAEd6IA1aaGCmwqYQRSZ3dKIROftiwiwN0M')
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def cmd_start(message: types.Message):
+    await message.answer("Приветствую, это эхо-бот!")
+
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
